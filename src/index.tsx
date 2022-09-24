b974849e2f1fbeaca13709d2cb545f64fe04edd9
Mohamed AboBakr <moabobakr@microsoft.com>
2022-09-24 07:08:47 +0200

    export more types

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,10 @@
  export {Labeler} from './labeler/Labeler';
 
  export type {LabelerRef} from './labeler/Labeler';
+
+ export type {LabelerProps} from './labeler/types/labelerProps';
+ 
+ export type { ISvgRenderer, ITokenStore, AnnotationData,
+    LineHeightCalculator, TokenPaddingCalculator, SvgRendererKind,
+    ISvgRendererPropsFactory,TokenEventListenersProp, GlobalEventExceptionPredicates, ITokenRendererProps } 
+from './labeler/types/labelerTypes';
